393771d04950ddf90c046f71a24210304596812b
kshitijrawat <shitij36363@gmail.com>
2022-10-17 15:56:54 +0530

    Updates

---
+++ precache-manifest.8249ec8b045e9034735a216253b7d61b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f68da34c593d3624b65",
+    "url": "/Text-opretions/static/css/main.45d40f31.chunk.css"
+  },
+  {
+    "revision": "8f68da34c593d3624b65",
+    "url": "/Text-opretions/static/js/main.8f68da34.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-opretions/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-opretions/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "75455fc16e4cee9f14e8",
+    "url": "/Text-opretions/static/js/runtime~main.75455fc1.js"
+  },
+  {
+    "revision": "2ca3d18a913a71e39d8bc395abf4f313",
+    "url": "/Text-opretions/index.html"
+  }
+];
